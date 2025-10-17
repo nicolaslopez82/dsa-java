a7c582d8d9e451b4436e42fb8ea1723a042e8330
nicolaslopez82 <nicolaslopez82@gmail.com>
2025-10-17 17:48:13 -0300

    Mehak Chopra Solution

---
+++ src/hyatt/CodingChallenger.java
@@ -0,0 +1,39 @@
+package hyatt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Task:
+ Assign values to the alphabets in an order such that:
+ a = 1, b = 2, c = 3, ..., z = 26
+ Then, take an alphabetical string as input and calculate the sum of the corresponding values of its characters.
+ Example:
+ Input: hello
+ Output: 52
+ Explanation: 8+5+12+12+15 = 52
+ */
+
+public class CodingChallenger {
+
+    public static Map<Character, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        map.put('a', 1);
+        map.put('b', 2);
+        map.put('b', 3);
+        String str = "abc";
+        System.out.println(returnTotalValue(str));
+    }
+
+    public static int returnTotalValue(String str){
+
+        Integer result = 0;
+
+        for(Integer index = 0; index < str.length() - 1; index++){
+            result += (int) map.get((Character)str.charAt(index));
+        }
+
+        return result;
+    }
+}
